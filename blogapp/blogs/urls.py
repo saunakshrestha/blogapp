8e089154a21a25126097eaa153414956e8f46a85
Saunak Shrestha <saunakshrestha@Saunaks-MacBook-Air.local>
2023-11-05 15:46:47 +0545

    Basic BLog App Completed

--- blogapp/blogs/urls.py
+++ blogapp/blogs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.blogs_index,name='blogs.index'),
+    path('',views.blogs_index,name='blogs_index'),
     path('post/<int:pk>/',views.blogs_detail,name='blogs_detail'),
     path('category/<str:category>/',views.blogs_category,name='blogs_category'),
 ]
